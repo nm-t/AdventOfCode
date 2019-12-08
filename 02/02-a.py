d2ca3cacf20a1a8cf55e4e6bc56015bb51b31bb4
nm-t <nathaliamtan@gmail.com>
2019-12-08 19:21:38 +1100

    [FEAT] Day 02 base

---
+++ 02/02-a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# IN:   Comma-separated values for an Intcode program
+# OUT:  Result of instructions from input
+
+def execute(instructions):
+    opcode = instructions[0]
+
+    #
+    if opcode == 1
+    # Continue until +4 off the instructions is unavail
+
+def add(instructions):
+    return True
+
+def multiply()
+    return True
+
+def cease()
+    return True
+
+# For each of the lines in the input
+file = open('input.txt', 'r')
+if file.mode == 'r':
+    for line in file:
+        input = line
+
+instructionArray = input.split(',')
+
+execute(instructionArray)
