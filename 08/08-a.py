1ec69ddbb75c3908cdfd79d8c482253d3b3f2a67
nm-t <nathaliamtan@gmail.com>
2019-12-09 20:11:03 +1100

    [FEAT] 08-a complete

--- 08/08-a.py
+++ 08/08-a.py
@@ -5,17 +5,63 @@
 
 width = 25
 height = 6
-layer = width * height
+layerSize = width * height
 
-layers = []
+def splitIntoLayers(input):
+    count = 0
+    numLayers = len(input)/layerSize
+    layers = []
 
-def checkCorruption(input):
-    numLayers = len(input)/layer
-    print:numLayers
+    start = 0
+    end = layerSize
+
+    currentSlice = ''
+
+    while count < numLayers:
+        currentSlice = input[start:end]
+        # Store each of the layers in the array
+        layers.insert(count, currentSlice)
+        start = end
+        end = end + layerSize
+        count = count + 1
+
+    checkCorruption(layers)
+
+def checkCorruption(layers):
+    lowestZeroCount = layerSize
+    lowestZeroLayer = ''
+
+    for layer in layers:
+        zeroCount = 0
+
+        for char in layer:
+            # print(char)
+            if (char == '0'):
+                zeroCount = zeroCount + 1
+
+        if zeroCount < lowestZeroCount:
+            lowestZeroCount = zeroCount
+            lowestZeroLayer = layer
+
+    # Lowest zero count layer has been found
+    # print(lowestZeroCount)
+    # print(lowestZeroLayer)
+
+    # Count the 1s and 2s in the layer with the lowest zero count
+    oneCount = 0
+    twoCount = 0
+    for char in lowestZeroLayer:
+        if (char == '1'):
+            oneCount = oneCount + 1
+        if (char == '2'):
+            twoCount = twoCount + 1
+
+    # print(lowestZeroCount + oneCount + twoCount)
+    print(oneCount * twoCount)
 
 # For each of the lines in the input
 file = open('input.txt', 'r')
 if file.mode == 'r':
     for line in file:
         input = line
-        checkCorruption(input)
+        splitIntoLayers(input)
